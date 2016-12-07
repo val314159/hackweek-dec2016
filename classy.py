bbcde7f6ebd0f0ae6f58028f4c0be00f17535dad
val314159 <jmward@gmail.com>
2016-12-07 23:54:12 +0000

    add niceities

--- classy.py
+++ classy.py
@@ -131,22 +131,11 @@ def create_graph():
 
 
 def run_inference_on_image(image):
-  """Runs inference on an image.
-
-  Args:
-    image: Image file name.
-
-  Returns:
-    Nothing
-  """
-  global predictions
+  global predictions, top_k
 
   if not tf.gfile.Exists(image):
     tf.logging.fatal('File does not exist %s', image)
   image_data = tf.gfile.FastGFile(image, 'rb').read()
-
-  # Creates graph from saved GraphDef.
-  create_graph()
 
   with tf.Session() as sess:
     # Some useful tensors:
@@ -160,6 +149,7 @@ def run_inference_on_image(image):
     softmax_tensor = sess.graph.get_tensor_by_name('softmax:0')
     predictions = sess.run(softmax_tensor,
                            {'DecodeJpeg/contents:0': image_data})
+    print(predictions)
     predictions = np.squeeze(predictions)
 
     # Creates node ID --> English string lookup.
@@ -197,19 +187,17 @@ def main(_):
     os.mkdir('static')
   except:
     pass
+  # Creates graph from saved GraphDef.
+  create_graph()
   maybe_download_and_extract()
+  print("OK NOW PRIME ONE")
   image = (FLAGS.image_file if FLAGS.image_file else
            os.path.join(FLAGS.model_dir, 'cropped_panda.jpg'))
-  print("OK NOW PRIME ONE")
   run_inference_on_image(image)
   print("OK NOW PRIME TWO")
   run_inference_on_image('default7.jpg')
   print("OK NOW SERVE WEB STUFF")
   run(host='',port=80)
-
-
-
-
 
 @route('/')
 def index():
@@ -219,6 +207,131 @@ def index():
     <input type="file" name="data" accept="video/*;capture=camcorder" />
 </form>
 ''']
+
+@route('/x')
+def index():
+  return ['''
+<html>
+<head>
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+</head>
+<body>
+
+<form method="POST" action="/upload" enctype="multipart/form-data">
+    <input type="text" name="name" value="default.jpg" />
+    <input type="file" name="data" accept="video/*;capture=camcorder" />
+</form>
+
+</body>
+</html>
+''']
+
+@route('/y')
+def _():
+  return ['''<!DOCTYPE html>
+<html lang="en">
+  <head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <link rel="icon" href="../../favicon.ico">
+
+  <script src="https://code.jquery.com/jquery.min.js"></script>
+
+  <!-- Latest compiled and minified CSS -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+  <!-- Optional theme -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+  <!-- Latest compiled and minified JavaScript -->
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+  <title>Narrow Jumbotron Template for Bootstrap</title>
+
+  <!-- Custom styles for this template -->
+  <link href="/static/jumbotron-narrow.css" rel="stylesheet">
+
+  <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+  <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  <![endif]-->
+  </head>
+
+  <body>
+
+    <div class="container">
+      <div class="header clearfix">
+        <nav>
+          <ul class="nav nav-pills pull-right">
+            <li role="presentation" class="active"><a href="#">Home</a></li>
+            <li role="presentation"><a href="#">About</a></li>
+            <li role="presentation"><a href="#">Contact</a></li>
+          </ul>
+        </nav>
+        <h3 class="text-muted">Project name</h3>
+      </div>
+
+<form method="POST" action="/upload" enctype="multipart/form-data">
+    <input type="text" name="name" value="default.jpg" />
+    <input type="file" name="data" accept="video/*;capture=camcorder" />
+</form>
+
+      <div class="jumbotron">
+        <h1>Jumbotron heading</h1>
+        <p class="lead">Cras justo odio, dapibus ac facilisis in, egestas eget quam. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+        <p><a class="btn btn-lg btn-success" href="#" role="button">Sign up today</a></p>
+      </div>
+
+      <div class="row marketing">
+        <div class="col-lg-6">
+          <h4>Subheading</h4>
+          <p>Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
+
+          <h4>Subheading</h4>
+          <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras mattis consectetur purus sit amet fermentum.</p>
+
+          <h4>Subheading</h4>
+          <p>Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
+        </div>
+
+        <div class="col-lg-6">
+          <h4>Subheading</h4>
+          <p>Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
+
+          <h4>Subheading</h4>
+          <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras mattis consectetur purus sit amet fermentum.</p>
+
+          <h4>Subheading</h4>
+          <p>Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
+        </div>
+      </div>
+
+      <footer class="footer">
+        <p>&copy; 2016 Company, Inc.</p>
+      </footer>
+
+    </div> <!-- /container -->
+
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+  </body>
+</html>
+''']
+
 
 @route('/static/<path:path>')
 def server_static(path):
@@ -255,18 +368,18 @@ def top_k_str2(top_k, html=True):
     if html:
       arr.append('<li>')
     if score >= 0.80:
-      arr.append("I'm certain it's a %s." % human_string)
+      certainty_str = "I'm certain it's a %s."
     elif score >= 0.30:
-      arr.append("I think it's a %s." % human_string)
+      certainty_str = "I think it's a %s."
     elif score >= 0.10:
-      arr.append("Maybe it's a %s." % human_string)
+      certainty_str = "Maybe it's a %s."
     elif score >= 0.01:
-      arr.append("I'm guessing it's a %s." % human_string)
+      certainty_str = "I'm guessing it's a %s."
     else:
-      arr.append("It's probably not a %s." % human_string)
+      certainty_str = "It's probably not a %s."
+    arr.append(certainty_str % human_string)
     arr.append('(score = %.5f)' % (score))
   return arr
-  
 
 if __name__ == '__main__':
   parser = argparse.ArgumentParser()
