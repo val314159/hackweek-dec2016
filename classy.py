d2404a2adfe359daaf4df826f038c878ecc9988a
val314159 <jmward@gmail.com>
2016-12-08 00:52:56 +0000

    make it big

--- classy.py
+++ classy.py
@@ -45,7 +45,7 @@ import numpy as np
 from six.moves import urllib
 import tensorflow as tf
 
-import os, sys, time
+import os, sys, time, bottle
 from bottle import route, run, template, static_file, request, response
 
 FLAGS = None
@@ -199,163 +199,38 @@ def main(_):
   print("OK NOW SERVE WEB STUFF")
   run(host='',port=80)
 
-@route('/')
-def index():
-  return ['''
-<form method="POST" action="/upload" enctype="multipart/form-data">
-    <input type="text" name="name" value="default.jpg" />
-    <input type="file" name="data" accept="video/*;capture=camcorder" />
-</form>
-''']
-
-@route('/x')
-def index():
-  return ['''
-<html>
-<head>
-<!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-
-<!-- Optional theme -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-
-<!-- Latest compiled and minified JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-
-</head>
-<body>
-
-<form method="POST" action="/upload" enctype="multipart/form-data">
-    <input type="text" name="name" value="default.jpg" />
-    <input type="file" name="data" accept="video/*;capture=camcorder" />
-</form>
-
-</body>
-</html>
-''']
-
-@route('/y')
-def _():
-  return ['''<!DOCTYPE html>
-<html lang="en">
-  <head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-  <meta name="description" content="">
-  <meta name="author" content="">
-  <link rel="icon" href="../../favicon.ico">
-
-  <script src="https://code.jquery.com/jquery.min.js"></script>
-
-  <!-- Latest compiled and minified CSS -->
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-
-  <!-- Optional theme -->
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-
-  <!-- Latest compiled and minified JavaScript -->
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-
-  <title>Narrow Jumbotron Template for Bootstrap</title>
-
-  <!-- Custom styles for this template -->
-  <link href="/static/jumbotron-narrow.css" rel="stylesheet">
-
-  <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-  <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-  <![endif]-->
-  </head>
-
-  <body>
-
-    <div class="container">
-      <div class="header clearfix">
-        <nav>
-          <ul class="nav nav-pills pull-right">
-            <li role="presentation" class="active"><a href="#">Home</a></li>
-            <li role="presentation"><a href="#">About</a></li>
-            <li role="presentation"><a href="#">Contact</a></li>
-          </ul>
-        </nav>
-        <h3 class="text-muted">Project name</h3>
-      </div>
-
-<form method="POST" action="/upload" enctype="multipart/form-data">
-    <input type="text" name="name" value="default.jpg" />
-    <input type="file" name="data" accept="video/*;capture=camcorder" />
-</form>
-
-      <div class="jumbotron">
-        <h1>Jumbotron heading</h1>
-        <p class="lead">Cras justo odio, dapibus ac facilisis in, egestas eget quam. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-        <p><a class="btn btn-lg btn-success" href="#" role="button">Sign up today</a></p>
-      </div>
-
-      <div class="row marketing">
-        <div class="col-lg-6">
-          <h4>Subheading</h4>
-          <p>Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
-
-          <h4>Subheading</h4>
-          <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras mattis consectetur purus sit amet fermentum.</p>
-
-          <h4>Subheading</h4>
-          <p>Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
-        </div>
-
-        <div class="col-lg-6">
-          <h4>Subheading</h4>
-          <p>Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
-
-          <h4>Subheading</h4>
-          <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras mattis consectetur purus sit amet fermentum.</p>
-
-          <h4>Subheading</h4>
-          <p>Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
-        </div>
-      </div>
-
-      <footer class="footer">
-        <p>&copy; 2016 Company, Inc.</p>
-      </footer>
-
-    </div> <!-- /container -->
-
-
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
-  </body>
-</html>
-''']
-
-
 @route('/static/<path:path>')
 def server_static(path):
   return static_file(path, root='./static/')
+
+@route('/')
+def server_static():
+  t = bottle.SimpleTemplate(name='index.tpl')
+  return t.render()
 
 @route('/upload', method='POST')
 def do_upload():
   name = request.forms.name
   data = request.files.data
-  if name and data and data.file:
-    pathname = 'static/'+name
-    new_pathname = 'static/'+time.ctime().replace(' ','-')+'.'+name
-    print("SAVING FILE:"+new_pathname)
-    data.save(new_pathname,overwrite=True)
-    filename = data.filename
-    print("WORKING ON PREDICTING")
-    top_k = run_inference_on_image(new_pathname)
-    print("DONE PREDICTING")
-    return """Hello %s! You uploaded %s (%s).
+  if not( name and data and data.file ):
+    return "You missed a field."
+
+  pathname = 'static/'+name
+  new_pathname = 'static/'+time.ctime().replace(' ','-')+'.'+name
+  print("SAVING FILE:"+new_pathname)
+  data.save(new_pathname,overwrite=True)
+  filename = data.filename
+  print("WORKING ON PREDICTING")
+  top_k = run_inference_on_image(new_pathname)
+  print("DONE PREDICTING")
+  msg = """Hello %s! You uploaded %s (%s).
 <a href="%s">%s</a> <hr> %s
 """ % (name, pathname, filename,
        new_pathname, new_pathname,
        top_k_str2(top_k))
-    return "You missed a field."
+  t = bottle.SimpleTemplate(name='upload')
+  return t.render(data=msg)
+
 
 def top_k_str(top_k):
   [print(x) for x in top_k_str2(top_k, html=False)]
